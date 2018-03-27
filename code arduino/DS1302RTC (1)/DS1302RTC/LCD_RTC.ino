0a7dc2de43cfbda142a81aa1ccba9abc9c789c7d
VaujanyAlexandre <34912608+VaujanyAlexandre@users.noreply.github.com>
2018-03-27 15:37:31 +0200

    Add files via upload
    Ce code affiche l'heure et le jour de la semaine sur l'écran LCD arduino.
    Le seul soucis est que lorsque l'on recompile le programme, il affiche l'heure et le jour de la semaine passés en paramètre au lieu d'afficher l'heure et le jour actuels.

---
+++ code arduino/DS1302RTC (1)/DS1302RTC/LCD_RTC.ino
@@ -0,0 +1,54 @@
+
+
+
+
+// DS1302:  RST pin    -> Arduino Digital 2
+//          DATA pin   -> Arduino Digital 3
+//          CLK pin  -> Arduino Digital 4
+// LCD:     DB7       -> Arduino Digital 6
+//          DB6       -> Arduino Digital 7
+//          DB5       -> Arduino Digital 8
+//          DB4       -> Arduino Digital 9
+//          E         -> Arduino Digital 10
+//          RS        -> Arduino Digital 11
+
+#include <LiquidCrystal.h>
+#include <DS1302.h>
+
+// Init the DS1302
+DS1302 rtc(2, 3, 4);
+
+// Init the LCD
+LiquidCrystal lcd(11, 10, 9, 8, 7, 6);
+
+void setup()
+{
+  // Set the clock to run-mode, and disable the write protection
+  rtc.halt(false);
+  rtc.writeProtect(false);
+
+
+  // Setup LCD to 16x2 characters
+  lcd.begin(16, 2);
+
+  // The following lines can be commented out to use the values already stored in the DS1302
+  rtc.setDOW(SUNDAY);
+  rtc.setTime(17, 35, 0);     // Set the time 
+  
+}
+
+void loop()
+{
+  // Display time centered on the upper line
+  lcd.setCursor(4, 0);
+  lcd.print(rtc.getTimeStr());
+  
+  
+  // Display date in the lower right corner
+  lcd.setCursor(4, 1);
+  lcd.print(rtc.getDOWStr());
+
+  // Wait one second before repeating :)
+  delay (1000);
+}
+
